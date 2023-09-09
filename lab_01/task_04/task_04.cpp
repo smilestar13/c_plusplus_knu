98987662a69de6ad330187f2e25af8012e6a1680
smilestar13 <oleksienko.boris@gmail.com>
2023-09-09 20:05:39 +0300

    lab_01/ task_04

--- lab_01/task_04/task_04.cpp
+++ lab_01/task_04/task_04.cpp
@@ -7,6 +7,10 @@ using namespace std;
 
 int main() {
     int x;
+    cout << "WARN: This program work only with int type.\n"
+            "\tIf you enter a different type of data,\n"
+            "\tthe behavior of the program may be unpredictable,\n"
+            "\tthe responsibility for this factor rests with you.\n\n";
     cout << "Enter a three-digit number: ";
     cin >> x;
     int num_1, num_2, num_3;
